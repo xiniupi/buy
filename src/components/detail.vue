8ccad9e5925570f282052914d2ece19b0d20a9b3
xiniupi <810175646@qq.com>
2019-03-28 15:46:43 +0800

    '发表评论完成'

--- src/components/detail.vue
+++ src/components/detail.vue
@@ -1,198 +1,262 @@
 <template>
-    <div>
-        <div class="section">
-            <div class="location">
-                <span>当前位置：</span>
-                <a href="/index.html">首页</a> &gt;
-                <a href="/goods.html">购物商城</a> &gt;
-                <a href="/goods/42/1.html">商品详情</a>
-            </div>
-        </div>
-        <div class="section">
-            <div class="wrapper clearfix">
-                <div class="wrap-box">
-                    <div class="left-925">
-                        <div class="goods-box clearfix">
-                            <div class="pic-box"><img :src="goodsinfo.img_url" alt=""></div>
-                            <div class="goods-spec">
-                                <h1>{{goodsinfo.title}}</h1>
-                                <p class="subtitle">{{goodsinfo.sub_title}}</p>
-                                <div class="spec-box">
-                                    <dl>
-                                        <dt>货号</dt>
-                                        <dd id="commodityGoodsNo">{{goodsinfo.goods_no}}</dd>
-                                    </dl>
-                                    <dl>
-                                        <dt>市场价</dt>
-                                        <dd>
-                                            <s id="commodityMarketPrice">¥{{goodsinfo.market_price}}</s>
-                                        </dd>
-                                    </dl>
-                                    <dl>
-                                        <dt>销售价</dt>
-                                        <dd>
-                                            <em id="commoditySellPrice" class="price">¥{{goodsinfo.sell_price}}</em>
-                                        </dd>
-                                    </dl>
-                                </div>
-                                <div class="spec-box">
-                                    <dl>
-                                        <dt>购买数量</dt>
-                                        <dd>
-                                            <div class="stock-box">
-                                               <el-input-number v-model="num1" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
-                                            </div>
-                                            <span class="stock-txt">
-                                                库存
-                                                <em id="commodityStockNum">{{goodsinfo.stock_quantity}}</em>件
-                                            </span>
-                                        </dd>
-                                    </dl>
-                                    <dl>
-                                        <dd>
-                                            <div id="buyButton" class="btn-buy">
-                                                <button onclick="cartAdd(this,'/',1,'/shopping.html');" class="buy">立即购买</button>
-                                                <button onclick="cartAdd(this,'/',0,'/cart.html');" class="add">加入购物车</button>
-                                            </div>
-                                        </dd>
-                                    </dl>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="goodsTabs" class="goods-tab bg-wrap">
-                            <div id="tabHead" class="tab-head" style="position: static; top: 517px; width: 925px;">
-                                <ul>
-                                    <li>
-                                        <a href="javascript:;" :class="{selected:index==1}" @click="index=1" >商品介绍</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;" :class="{selected:index==2}" @click="index=2" >商品评论</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="tab-content entry"  v-show="index==1" v-html="goodsinfo.content">
-                                
-                            </div>
-                            <div  class="tab-content"  v-show="index==2" >
-                                <div class="comment-box">
-                                    <div id="commentForm" name="commentForm" class="form-box">
-                                        <div class="avatar-box">
-                                            <i class="iconfont icon-user-full"></i>
-                                        </div>
-                                        <div class="conn-box">
-                                            <div class="editor">
-                                                <textarea id="txtContent" name="txtContent" sucmsg=" " datatype="*10-1000" nullmsg="请填写评论内容！"></textarea>
-                                                <span class="Validform_checktip"></span>
-                                            </div>
-                                            <div class="subcon">
-                                                <input id="btnSubmit" name="submit" type="submit" value="提交评论" class="submit">
-                                                <span class="Validform_checktip"></span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <ul id="commentList" class="list-box">
-                                        <p style="margin: 5px 0px 15px 69px; line-height: 42px; text-align: center; border: 1px solid rgb(247, 247, 247);">暂无评论，快来抢沙发吧！</p>
-                                        <li>
-                                            <div class="avatar-box">
-                                                <i class="iconfont icon-user-full"></i>
-                                            </div>
-                                            <div class="inner-box">
-                                                <div class="info">
-                                                    <span>匿名用户</span>
-                                                    <span>2017/10/23 14:58:59</span>
-                                                </div>
-                                                <p>testtesttest</p>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="avatar-box">
-                                                <i class="iconfont icon-user-full"></i>
-                                            </div>
-                                            <div class="inner-box">
-                                                <div class="info">
-                                                    <span>匿名用户</span>
-                                                    <span>2017/10/23 14:59:36</span>
-                                                </div>
-                                                <p>很清晰调动单很清晰调动单</p>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                    <div class="page-box" style="margin: 5px 0px 0px 62px;">
-                                        <div id="pagination" class="digg">
-                                            <span class="disabled">« 上一页</span>
-                                            <span class="current">1</span>
-                                            <span class="disabled">下一页 »</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="left-220">
-                        <div class="bg-wrap nobg">
-                            <div class="sidebar-box">
-                                <h4>推荐商品</h4>
-                                <ul class="side-img-list">
-                                    <li v-for="item in hotgoodslist">
-                                        <div class="img-box">
-                                                <router-link :to="'/detail/'+item.id">
-                                            <!-- <a href="#/site/goodsinfo/90" class=""> -->
-                                                <img :src="item.img_url">
-                                            <!-- </a> -->
-                                            </router-link>
-                                        </div>
-                                        <div class="txt-box">
-                                            <router-link :to="'/detail/'+item.id">{{item.title}}   </router-link>
-                                            <span>{{item.add_time | globalFormatTime}}</span>
-                                        </div> 
-                                    </li>
-                                   
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div>
+    <div class="section">
+      <div class="location">
+        <span>当前位置：</span>
+        <a href="/index.html">首页</a> &gt;
+        <a href="/goods.html">购物商城</a> &gt;
+        <a href="/goods/42/1.html">商品详情</a>
+      </div>
     </div>
+    <div class="section">
+      <div class="wrapper clearfix">
+        <div class="wrap-box">
+          <div class="left-925">
+            <div class="goods-box clearfix">
+              <div class="pic-box">
+                <el-carousel >
+                  <el-carousel-item v-for="(item,index) in imglist" :key="index">
+                    <img :src="item.thumb_path" alt="">
+                  </el-carousel-item>
+                </el-carousel>
+              </div>
+              <div class="goods-spec">
+                <h1>{{goodsinfo.title}}</h1>
+                <p class="subtitle">{{goodsinfo.sub_title}}</p>
+                <div class="spec-box">
+                  <dl>
+                    <dt>货号</dt>
+                    <dd id="commodityGoodsNo">{{goodsinfo.goods_no}}</dd>
+                  </dl>
+                  <dl>
+                    <dt>市场价</dt>
+                    <dd>
+                      <s id="commodityMarketPrice">¥{{goodsinfo.market_price}}</s>
+                    </dd>
+                  </dl>
+                  <dl>
+                    <dt>销售价</dt>
+                    <dd>
+                      <em id="commoditySellPrice" class="price">¥{{goodsinfo.sell_price}}</em>
+                    </dd>
+                  </dl>
+                </div>
+                <div class="spec-box">
+                  <dl>
+                    <dt>购买数量</dt>
+                    <dd>
+                      <div class="stock-box">
+                        <el-input-number
+                          v-model="num1"
+                          @change="handleChange"
+                          :min="1"
+                          :max="10"
+                          label="描述文字"
+                        ></el-input-number>
+                      </div>
+                      <span class="stock-txt">
+                        库存
+                        <em id="commodityStockNum">{{goodsinfo.stock_quantity}}</em>件
+                      </span>
+                    </dd>
+                  </dl>
+                  <dl>
+                    <dd>
+                      <div id="buyButton" class="btn-buy">
+                        <button onclick="cartAdd(this,'/',1,'/shopping.html');" class="buy">立即购买</button>
+                        <button onclick="cartAdd(this,'/',0,'/cart.html');" class="add">加入购物车</button>
+                      </div>
+                    </dd>
+                  </dl>
+                </div>
+              </div>
+            </div>
+            <div id="goodsTabs" class="goods-tab bg-wrap">
+              <div
+                id="tabHead"
+                class="tab-head"
+                style="position: static; top: 517px; width: 925px;"
+              >
+                <ul>
+                  <li>
+                    <a href="javascript:;" :class="{selected:index==1}" @click="index=1">商品介绍</a>
+                  </li>
+                  <li>
+                    <a href="javascript:;" :class="{selected:index==2}" @click="index=2">商品评论</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="tab-content entry" v-show="index==1" v-html="goodsinfo.content"></div>
+              <div class="tab-content" v-show="index==2">
+                <div class="comment-box">
+                  <div id="commentForm" name="commentForm" class="form-box">
+                    <div class="avatar-box">
+                      <i class="iconfont icon-user-full"></i>
+                    </div>
+                    <div class="conn-box">
+                      <div class="editor">
+                        <textarea
+                          id="txtContent"
+                          name="txtContent"
+                          sucmsg=" "
+                          data-type="*10-1000"
+                          nullmsg="请填写评论内容！"
+                          v-model.trim="comment"
+                        ></textarea>
+                        <span class="Validform_checktip"></span>
+                      </div>
+                      <div class="subcon">
+                        <input
+                          id="btnSubmit"
+                          name="submit"
+                          type="submit"
+                          value="提交评论"
+                          class="submit"
+                          @click="postcomment"
+                        >
+                        <span class="Validform_checktip"></span>
+                      </div>
+                    </div>
+                  </div>
+                  <ul id="commentList" class="list-box">
+                    <p
+                      style="margin: 5px 0px 15px 69px; line-height: 42px; text-align: center; border: 1px solid rgb(247, 247, 247);"
+                    >暂无评论，快来抢沙发吧！</p>
+                    <li>
+                      <div class="avatar-box">
+                        <i class="iconfont icon-user-full"></i>
+                      </div>
+                      <div class="inner-box">
+                        <div class="info">
+                          <span>匿名用户</span>
+                          <span>2017/10/23 14:58:59</span>
+                        </div>
+                        <p>testtesttest</p>
+                      </div>
+                    </li>
+                    <li>
+                      <div class="avatar-box">
+                        <i class="iconfont icon-user-full"></i>
+                      </div>
+                      <div class="inner-box">
+                        <div class="info">
+                          <span>匿名用户</span>
+                          <span>2017/10/23 14:59:36</span>
+                        </div>
+                        <p>很清晰调动单很清晰调动单</p>
+                      </div>
+                    </li>
+                  </ul>
+                  <div class="page-box" style="margin: 5px 0px 0px 62px;">
+                    <div id="pagination" class="digg">
+                      <span class="disabled">« 上一页</span>
+                      <span class="current">1</span>
+                      <span class="disabled">下一页 »</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="left-220">
+            <div class="bg-wrap nobg">
+              <div class="sidebar-box">
+                <h4>推荐商品</h4>
+                <ul class="side-img-list">
+                  <li v-for="item in hotgoodslist">
+                    <div class="img-box">
+                      <router-link :to="'/detail/'+item.id">
+                        <!-- <a href="#/site/goodsinfo/90" class=""> -->
+                        <img :src="item.img_url">
+                        <!-- </a> -->
+                      </router-link>
+                    </div>
+                    <div class="txt-box">
+                      <router-link :to="'/detail/'+item.id">{{item.title}}</router-link>
+                      <span>{{item.add_time | globalFormatTime}}</span>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 // import axios from 'axios';
 export default {
-    name:"detail",
-    data(){
-        return {
-            goodsinfo:{},
-            index:1,
-           hotgoodslist:[],
-           num1:1
-        }
+  name: "detail",
+  data() {
+    return {
+      goodsinfo: {},
+      index: 1,
+      hotgoodslist: [],
+      num1: 1,
+      imglist:[],
+      comment:""
+    };
+  },
+  methods: {
+    getDetail() {
+      this.$axios
+        .get(`/site/goods/getgoodsinfo/${this.$route.params.id}`)
+        .then(res => {
+          console.log(res);
+          this.goodsinfo = res.data.message.goodsinfo;
+          this.hotgoodslist = res.data.message.hotgoodslist;
+          this.imglist = res.data.message.imglist;
+        });
     },
-    methods: {
-        getDetail(){
-            this.$axios.get(`/site/goods/getgoodsinfo/${this.$route.params.id}`).then(res=>{
-            console.log(res);
-            this.goodsinfo = res.data.message.goodsinfo;
-            this.hotgoodslist = res.data.message.hotgoodslist;
-        })
-        },
-         handleChange(value) {
-         
+    handleChange(value) {},
+     postcomment(){
+      if(this.comment===""){
+           this.$message.error('请输入评论内容');
+      }else{
+          this.$axios.post(`site/validate/comment/post/goods/${this.$route.params.id}`,{
+              "commenttxt":this.comment
+          }).then(res=>{
+              if(res.data.status===0){
+                   this.$message.success(res.data.message);
+               this.comment ='';
+              }
+              
+          })
       }
-    },
-    created(){
-        this.getDetail()
-    },
-    // 侦听器的作用是监测值改变
-    watch: {
-        $route(value,oldvalue){
-           this.getDetail()
-        }
-    },
-}
+  },
+  },
+  created() {
+    this.getDetail();
+  },
+ 
+  // 侦听器的作用是监测值改变
+  watch: {
+    $route(value, oldvalue) {
+      this.getDetail();
+    }
+  }
+};
 </script>
 
 <style>
-
+.pic-box{
+    width: 395px;
+    height: 320px;
+}
+.pic-box .el-carousel{
+    width: 100%;
+    height: 100%;
+}
+.pic-box .el-carousel__container{
+    width: 100%;
+    height: 100%;
+}
+.pic-box .el-carousel__container img{
+    width: 100%;
+    height: 100%;
+    display: block;
+}
 </style>
